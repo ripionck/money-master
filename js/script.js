02338f17d41fe560a37dc6dd27c9b4a21e21c715
ripionck <ripionchakma.421@gmail.com>
2022-02-17 15:38:00 +0600

    common function created

--- js/script.js
+++ js/script.js
@@ -1,31 +1,47 @@
+function getInputValue(itemId) {
+    const itemField = document.getElementById(itemId);
+    const itemAmount = parseFloat(itemField.value);
+
+    //clear input field
+    itemField.value = "";
+    return itemAmount;
+}
+
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const incomeField = document.getElementById('income-field');
-    const foodField = document.getElementById('food-field');
-    const rentfield = document.getElementById('rent-field');
-    const clothesField = document.getElementById('clothes-field');
+    /*  const incomeField = document.getElementById('income-field');
+     const foodField = document.getElementById('food-field');
+     const rentfield = document.getElementById('rent-field');
+     const clothesField = document.getElementById('clothes-field');
+ 
+     const incomeAmount = parseFloat(incomeField.value);
+     const foodAmount = parseFloat(foodField.value);
+     const rentAmount = parseFloat(rentfield.value);
+     const clothesAmount = parseFloat(clothesField.value);
+  */
+    const incomeAmount = getInputValue('income-field');
+    const foodAmount = getInputValue('food-field');
+    const rentAmount = getInputValue('rent-field');
+    const clothesAmount = getInputValue('clothes-field');
 
-    const incomeAmount = incomeField.value;
-    const foodAmount = foodField.value;
-    const rentAmount = rentfield.value;
-    const clothesAmount = clothesField.value;
-
-
-    const totalExpenses = parseFloat(foodAmount) + parseFloat(rentAmount) + parseFloat(clothesAmount);
+    //total expenses
+    const totalExpenses = foodAmount + rentAmount + clothesAmount;
 
     const total = document.getElementById('total-expenses');
     total.innerText = totalExpenses;
 
-    const balanceAmount = parseFloat(incomeAmount) - parseFloat(totalExpenses);
+    //balance
+    const balanceAmount = incomeAmount - totalExpenses;
 
     const balance = document.getElementById('balance');
     balance.innerText = balanceAmount;
 
-
-    incomeField.value = '';
-    foodField.value = '';
-    rentfield.value = '';
-    clothesField.value = '';
+    /*  //clear input field
+     incomeField.value = '';
+     foodField.value = '';
+     rentfield.value = '';
+     clothesField.value = '';
+  */
 })
 
 document.getElementById('save-btn').addEventListener('click', function () {
@@ -33,16 +49,18 @@ document.getElementById('save-btn').addEventListener('click', function () {
     const saveRateField = document.getElementById('save-rate');
     const saveRateAmount = saveRateField.value;
 
-    const saveRate = (parseFloat(balance.innerText) * parseFloat(saveRateAmount)) / 100;
-
+    //saving amount
+    let saveRate = (parseFloat(balance.innerText) * parseFloat(saveRateAmount)) / 100;
 
     const savingAmount = document.getElementById('saving-amount');
     savingAmount.innerText = saveRate;
 
+    //remaining balance
     const remainingBalanceAmount = parseFloat(balance.innerText) - parseFloat(savingAmount.innerText);
 
     const remainingBalance = document.getElementById('remaining-balance');
     remainingBalance.innerText = remainingBalanceAmount;
 
+    //clear save rate input
     saveRateField.value = '';
 })
