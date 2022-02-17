4bf4993e975a2024e0e80273277f81e317b16d99
ripionck <ripionchakma.421@gmail.com>
2022-02-17 21:33:45 +0600

    error handle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function getInputValue(itemId) {
     const itemField = document.getElementById(itemId);
     const itemAmount = parseFloat(itemField.value);
-    if (itemAmount < 0 || isNaN(itemAmount)) {
-        return alert('Please enter a valid number!')
+    if (isNaN(itemAmount) || itemAmount < 0) {
+        itemField.value = "";
+        return alert('Please input positive number!')
     }
     //clear input field
-    itemField.value = "";
+    itemField.value = "00";
     return itemAmount;
 }
 
@@ -20,16 +21,19 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const totalExpenses = foodAmount + rentAmount + clothesAmount;
 
     const total = document.getElementById('total-expenses');
-    total.innerText = totalExpenses;
+    total.innerText = totalExpenses.toFixed(2);
+    total.innerText = "00";
+
     if (incomeAmount < total.innerText) {
         return alert('Insufficient balance!');
     }
     else {
         //balance
-        const balanceAmount = incomeAmount - totalExpenses;
+        const balanceAmount = incomeAmount - parseFloat(totalExpenses);
 
         const balance = document.getElementById('balance');
-        balance.innerText = balanceAmount;
+        balance.innerText = balanceAmount.toFixed(2);
+        balance.innerText = "00";
     }
 
 });
@@ -42,21 +46,21 @@ document.getElementById('save-btn').addEventListener('click', function () {
     //clear save rate input
     saveRateField.value = '';
 
-    if (saveRateAmount < 0 || saveRateAmount > 100) {
-        return alert('Please insert a valid number less than hundred!')
+    if (saveRateAmount < 0 || isNaN(saveRateAmount) || saveRateAmount > 100) {
+        return alert('You can not save more than your balance.')
     }
     else {
         //saving amount
         const saveRate = (parseFloat(balance.innerText) * parseFloat(saveRateAmount)) / 100;
 
         const savingAmount = document.getElementById('saving-amount');
-        savingAmount.innerText = saveRate;
+        savingAmount.innerText = saveRate.toFixed(2);
 
         //remaining balance
         const remainingBalanceAmount = parseFloat(balance.innerText) - parseFloat(savingAmount.innerText);
 
         const remainingBalance = document.getElementById('remaining-balance');
-        remainingBalance.innerText = remainingBalanceAmount;
+        remainingBalance.innerText = remainingBalanceAmount.toFixed(2);
     }
 
 })
